ff4fbcf483fda5726c484e121731098a0dbb10d3
lia-na-2210 <naneko.bl@gmail.com>
2022-12-03 19:22:35 -0600

    created the correct event listener for the buttons, only score counting pending

--- index.js
+++ index.js
@@ -1,57 +1,69 @@
-const choices = ['rock', 'paper', 'scissors']
-const buttons = document.getElementById('btn-pick')
-
-document.addEventListener('click', (e) => {
-    let pick = e.target.innerText
-    console.log(pick)
-});
+const choices = ['Rock', 'Paper', 'Scissors'];
+const buttons = document.getElementsByClassName('btn-pick');
+const plyChoice = document.getElementById('ply-choice');
+const comChoice = document.getElementById('com-choice');
+const playBtn = document.getElementById('btn-play');
 
 
 
-/*btnArray.forEach(button => button.addEventListener('click', () => {
-    let playerPick = button.innerText
-    console.log(playerPick)
-}))*/
-
-
-
-function game(){
-    let playerScore = 0
-    let compuScore = 0
-
-    /*for (let i = 0; i < 5; i++ ) {
-        let winner = playRound()
-        if (winner == 'player') {
-            playerScore += 1
-            console.log('The player won!')
-        } else if (winner == 'compu') {
-            compuScore += 1
-            console.log('The computer won!')
-        } else {
-            console.log("It's a tie!")
-        }
-    }*/
-    console.log(`The player won ${playerScore} times and the computer ${compuScore}`)
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', playerChoice);
 }
 
-function playRound () {
-    const computerChoice = getComputerChoice();
-    const playerChoice = getPlayerChoice();
-    console.log(playerChoice)
-    console.log(computerChoice)
-    const winner = checkWinner(playerChoice, computerChoice);
-    return winner
+function playerChoice(obj) {
+    let playerPick = obj.target.innerText;
+    //console.log(playerPick)
+    plyChoice.innerText = playerPick;
+}
+
+
+function game(winner){
+    let winnerText = document.getElementById('winner');
+
+    let playerScore = 0;
+    let compuScore = 0;
+    
+
+    if (winner == 'player') {
+        playerScore += 1;
+        winnerText.innerText = 'The player won!';
+    } else if (winner == 'compu') {
+        compuScore += 1;
+        winnerText.innerText = 'The computer won!';
+    } else {
+        winnerText.innerText = "It's a tie!";
+    }
+    
+    console.log(playerScore, compuScore);
+
+    /*let playerPoints = document.getElementById('player-score');
+    playerPoints.innerText += playerScore;
+    let compuPoints = document.getElementById('computer-score');
+    compuPoints.innerText += compuScore;*/
     
 }
 
-function checkWinner(player, compu) {
-    if (player == compu) {
+function playRound (player) {
+    let computerPick = getComputerChoice();
+    let playerPick = plyChoice.innerText;
+    comChoice.innerText = computerPick;
+
+    const winner = checkWinner(playerPick, computerPick);
+    console.log(winner);
+    game(winner);
+    
+}
+
+function checkWinner(p, c) {
+    console.log(p);
+    console.log(c);
+    if (p == c) {
         return('tie');
-    } else if (player == 'paper' && compu == 'rock') {
+    } else if (p == 'Paper' && c == 'Rock') {
         return('player')
-    } else if (player == 'scissors' && compu == 'paper') {
+    } else if (p == 'Scissors' && c == 'Paper') {
         return('player')
-    } else if (player == 'rock' && compu == 'scissors') {
+    } else if (p == 'Rock' && c == 'Scissors') {
         return('player')
     } else {
         return('compu')
@@ -59,34 +71,10 @@ function checkWinner(player, compu) {
 }
 
 function getComputerChoice () {
-    return choices[Math.floor(Math.random()*choices.length)]
+    return choices[Math.floor(Math.random()*choices.length)];
     
 }
 
-
-/*function getPlayerChoice(pick) { 
-    
-    /*let input = prompt('Pick rock, paper or scissors: ')
-    while (input == null) {
-        input = prompt('Pick rock, paper or scissors: ')
-    }
-    input = input.toLowerCase()
-    let check = validateChoice(input)*/
-    /*console.log(check)*/
-    /*while (check == false) {
-        input = prompt('You can only type rock, paper or scissors, so pick: ')
-        while (input == null) {
-            input = prompt('Pick rock, paper or scissors: ')
-        }
-        input = input.toLowerCase()
-        check = validateChoice(input)
-    }
-    console.log(playerPick)
-    return playerPick
-}*/
-
-function validateChoice(choice) {
-    return choices.includes(choice)
-}
-
-game()
+playBtn.addEventListener('click', function () {
+    playRound();
+});
